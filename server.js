057b1b9970eb2b29571d25ce350ae7349aafb852
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-01-25 11:23:34 +0100

    Update the package.json with new import and connection etablished

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
+const cors = require('cors');
+
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+    .then(() => console.log("CONNECTED"));
+
+mongoose.connection.on('error', err => {
+    console.log(`CONNECTION FAILED : ${err.message}`)
+});
 const app = express();
+
 
 port = process.env.PORT || 3030;
 app.listen(port, () => {
